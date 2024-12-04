ad703ff7b90bfd5f30f76b7891af792deda41605
yamazaki <aoi.yamazaki0621@gmail.com>
2024-12-04 10:10:38 +0900

    No.023,041 画像の削除機能実装とそれに伴う並び替え処理のロジックを修正

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import ProductCreateView, ProductEditView, ProductListView, ProductDetailView, ProductDeleteView, toggle_favorite, favorite_list
+from .views import ProductCreateView, ProductEditView, ProductListView, ProductDetailView, ProductDeleteView, toggle_favorite, favorite_list, delete_images
 
 app_name = 'products'
 
@@ -10,6 +10,7 @@ urlpatterns = [
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('<int:pk>/edit/', ProductEditView.as_view(), name='product_edit'),
     path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+    path('image/<int:image_id>/delete/', delete_images, name='delete_images'),
     path('<int:product_id>/toggle_favorite/', toggle_favorite, name='toggle_favorite'),
     path('favorites/', favorite_list, name='favorite_list'),
 ]
