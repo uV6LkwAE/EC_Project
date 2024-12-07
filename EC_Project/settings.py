5f4d9c9213032902fc1b514594e437e474743f22
yamazaki <aoi.yamazaki0621@gmail.com>
2024-12-07 12:38:01 +0900

    env追加

--- EC_Project/settings.py
+++ EC_Project/settings.py
@@ -19,11 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$u6uq2w+fuud3a0j2(b*5--e4jvn)w13_x$$m%m!nt*%y*-nta'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
