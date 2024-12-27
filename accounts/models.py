f207e7375998fe1901fa64b04cbc4ae6cf855a3b
yamazaki <aoi.yamazaki0621@gmail.com>
2024-12-27 19:22:08 +0900

    No.083 モデルからNickNameを削除

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,6 @@ from django.core.exceptions import ValidationError
 
 # カスタムユーザーモデルの定義
 class CustomUser(AbstractUser):
-    nickname = models.CharField(max_length=150, blank=False, null=False, help_text='必須です。ニックネームを入力してください。')
     age = models.IntegerField(blank=True, null=True, help_text='任意です。年齢を入力してください。')
     address = models.CharField(max_length=255, blank=True, null=True, help_text='任意です。住所を入力してください。')
     icon = models.ImageField(upload_to='user_icons/', blank=True, null=True, help_text='任意です。アイコン画像をアップロードしてください。')
